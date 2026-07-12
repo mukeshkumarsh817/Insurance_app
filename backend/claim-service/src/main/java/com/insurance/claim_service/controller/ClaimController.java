cb00b7e1bfd766fc99296fae0e84b0a3c5cb6b3d
mukeshkumar <mukeshkumarsh817@gmail.com>
2026-07-12 13:48:34 +0530

    New Change

--- backend/claim-service/src/main/java/com/insurance/claim_service/controller/ClaimController.java
+++ backend/claim-service/src/main/java/com/insurance/claim_service/controller/ClaimController.java
@@ -27,6 +27,7 @@ public class ClaimController {
     public ResponseEntity<ClaimResponseDTO> addClaim(
             @Valid @RequestBody ClaimRequestDTO request) {
         //System.out.println("Hello World");
+        //System.out.println("Hello World");
         ClaimResponseDTO response = claimService.addClaim(request);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
