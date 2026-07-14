bcb21ed67e5c578068bc3f7b83b922da30573588
mukeshkumar <mukeshkumarsh817@gmail.com>
2026-07-14 13:58:37 +0530

    Ashsih Kumar added

--- backend/claim-service/src/main/java/com/insurance/claim_service/controller/ClaimController.java
+++ backend/claim-service/src/main/java/com/insurance/claim_service/controller/ClaimController.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.insurance.claim_service.dto.ClaimRequestDTO;
 import com.insurance.claim_service.dto.ClaimResponseDTO;
@@ -13,6 +20,7 @@ import com.insurance.claim_service.service.ClaimService;
 import jakarta.validation.Valid;
 //import jakarta.validation.Valid;
 //System.out.println("mksh");
+//System.out.println("Ashish Kumar");
 @RestController
 @RequestMapping("/api/claims")
 public class ClaimController {
