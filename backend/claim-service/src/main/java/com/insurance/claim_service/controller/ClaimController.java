9c82d4f5cf8d47f85570f098005fdf306e0f5c87
mukeshkumar <mukeshkumarsh817@gmail.com>
2026-07-14 14:00:29 +0530

    Revert "Ashsih Kumar added"
    This reverts commit bcb21ed67e5c578068bc3f7b83b922da30573588.

--- backend/claim-service/src/main/java/com/insurance/claim_service/controller/ClaimController.java
+++ backend/claim-service/src/main/java/com/insurance/claim_service/controller/ClaimController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.insurance.claim_service.dto.ClaimRequestDTO;
 import com.insurance.claim_service.dto.ClaimResponseDTO;
@@ -20,7 +13,6 @@ import com.insurance.claim_service.service.ClaimService;
 import jakarta.validation.Valid;
 //import jakarta.validation.Valid;
 //System.out.println("mksh");
-//System.out.println("Ashish Kumar");
 @RestController
 @RequestMapping("/api/claims")
 public class ClaimController {
